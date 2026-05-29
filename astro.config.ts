d19faebb9ad7042c6854f9e39f4385b26125c8ad
Matilde Morrone <contact@matilde.pet>
2026-05-29 11:25:41 +0200

    Format code

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,18 @@
-import sitemap from "@astrojs/sitemap";
-import { defineConfig, fontProviders, svgoOptimizer } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders, svgoOptimizer } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	site: "https://matilde.pet",
+	site: 'https://matilde.pet',
 	integrations: [sitemap()],
-	fonts: [{
-		provider: fontProviders.fontsource(),
-		name: "Fredoka",
-		cssVariable: "--fredoka",
-		weights: ["500 600 700"],
-	}],
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: 'Fredoka',
+			cssVariable: '--fredoka',
+			weights: ['500 600 700'],
+		},
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},
