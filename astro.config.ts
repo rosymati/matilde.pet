29e3f5a89c91783e1b37c069fbc0f76092822790
Matilde Morrone <contact@matilde.pet>
2026-05-29 11:20:34 +0200

    Use astro font provider

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,16 @@
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, svgoOptimizer } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders, svgoOptimizer } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	site: 'https://matilde.pet',
+	site: "https://matilde.pet",
 	integrations: [sitemap()],
+	fonts: [{
+		provider: fontProviders.fontsource(),
+		name: "Fredoka",
+		cssVariable: "--fredoka",
+		weights: ["500 600 700"],
+	}],
 	vite: {
 		plugins: [tailwindcss()],
 	},
